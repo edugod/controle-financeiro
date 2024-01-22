9963cbe2ed8bd6a01c14aff3ac001e9e22484876
Eduardo Lange <eduardoandrelange@gmail.com>
2024-01-21 21:22:18 -0300

    bora y bora

--- src/Components/HistoricoDespesas.jsx
+++ src/Components/HistoricoDespesas.jsx
@@ -1,16 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 import despesaService from '../controllers/despesas';
 
 const HistoricoDespesas = ({ despesas, setDespesas }) => {
+  const [showDeleteModal, setShowDeleteModal] = useState(false);
+  const [deletingDespesaId, setDeletingDespesaId] = useState(null);
+
   const handleDelete = async (despesaId) => {
+    // Abre o modal de confirmação
+    setShowDeleteModal(true);
+    setDeletingDespesaId(despesaId);
+  };
+
+  const confirmDelete = async () => {
     try {
       // Chama a função de delete no serviço
-      await despesaService.deleteDespesa(despesaId);
+      await despesaService.deleteDespesa(deletingDespesaId);
+
       // Atualiza o estado para refletir a exclusão no frontend
-      setDespesas((prevDespesas) => prevDespesas.filter((despesa) => despesa.id !== despesaId));
+      setDespesas((prevDespesas) => prevDespesas.filter((despesa) => despesa.id !== deletingDespesaId));
+
+      // Fecha o modal de confirmação
+      setShowDeleteModal(false);
+      setDeletingDespesaId(null);
     } catch (error) {
+      // Lida com erros
       console.error('Erro ao deletar despesa:', error);
     }
+  };
+
+  const cancelDelete = () => {
+    // Fecha o modal de confirmação
+    setShowDeleteModal(false);
+    setDeletingDespesaId(null);
   };
 
   return (
@@ -25,6 +46,15 @@ const HistoricoDespesas = ({ despesas, setDespesas }) => {
           </li>
         ))}
       </ul>
+
+      {/* Modal de Confirmação */}
+      {showDeleteModal && (
+        <div className="modal">
+          <p>Tem certeza que deseja deletar?</p>
+          <button onClick={confirmDelete}>Sim</button>
+          <button onClick={cancelDelete}>Cancelar</button>
+        </div>
+      )}
     </div>
   );
 };
