9edaeea675061a122c65289189ebd1efa2368b66
Eduardo Lange <eduardoandrelange@gmail.com>
2024-01-25 21:36:46 -0300

    bumbou

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,37 +1,42 @@
-import React, { useState, useEffect } from 'react';
-import HistoricoDespesas from '../Components/HistoricoDespesas';
-import DespesaForm from '../Components/DespesaForm';
-import SaldoMensal from '../Components/SaldoMensal';
-import despesaService from '../controllers/despesas';
+import React, { useState, useEffect } from 'react'
+import HistoricoDespesas from '../Components/HistoricoDespesas'
+import DespesaForm from '../Components/DespesaForm'
+import SaldoMensal from '../Components/SaldoMensal'
+import despesaService from '../controllers/despesas'
 
 const HomePage = () => {
-  const [despesas, setDespesas] = useState([]);
-  const [saldo, setSaldo] = useState(0);
+	const [despesas, setDespesas] = useState([])
+	const [saldo, setSaldo] = useState(0)
 
-  useEffect(() => {
-    despesaService.getAll().then(response => {
-      setDespesas(response.data)
-    }).catch(error => {
-      console.error("Error fetching despesas:", error);
-    })
-  },[])
+	// useEffect para carregar dados iniciais
+	useEffect(() => {
+		despesaService
+			.getAll()
+			.then((response) => {
+				setDespesas(response.data)
+			})
+			.catch((error) => {
+				console.error('Error fetching despesas:', error)
+			})
+	}, [])
 
-  const handleAddDespesa = (novaDespesa) => {
-    setDespesas([...despesas, { id: despesas.length + 1, ...novaDespesa }]);
-    // Atualiza o saldo com base na nova despesa
-    console.log('despesas :>> ', despesas);
-    setSaldo(saldo - novaDespesa.valor);
-    console.log('deposi de add :>> ', despesas);
+	// useEffect para calcular o saldo com base nas despesas
+	useEffect(() => {
+		const saldoTotal = despesas.reduce((total, despesa) => total - despesa.valor, 0)
+		setSaldo(saldoTotal)
+	}, [despesas])
 
-  };
-  
-  return (
-    <div>
-      <DespesaForm onAddDespesa={handleAddDespesa} />
-      <SaldoMensal saldo={saldo} />
-      <HistoricoDespesas despesas={despesas} setDespesas={setDespesas} />
-    </div>
-  );
-};
+	const handleAddDespesa = (novaDespesa) => {
+		setDespesas([...despesas, { id: despesas.length + 1, ...novaDespesa }])
+	}
 
-export default HomePage;
+	return (
+		<div>
+			<DespesaForm onAddDespesa={handleAddDespesa} />
+			<SaldoMensal saldo={saldo} />
+			<HistoricoDespesas despesas={despesas} setDespesas={setDespesas} />
+		</div>
+	)
+}
+
+export default HomePage
