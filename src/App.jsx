e6665795928c4dcf229db520e6d335d602425887
Eduardo Lange <eduardoandrelange@gmail.com>
2024-02-05 21:44:09 -0300

    login test

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import HomePage from './pages/HomePage';
+import Login from './pages/Login';
 
 
 const App = () => {
   return (
     <Router>
       <Routes>
+        <Route path="/login" element={<Login />} />
         <Route path="/" element={<HomePage />} />
       </Routes>
     </Router>
