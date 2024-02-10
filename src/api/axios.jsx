9f72ae4d4ce68874b4427862d858bb32087b6803
Eduardo Lange <eduardoandrelange@gmail.com>
2024-02-09 21:09:56 -0300

    Update axios.jsx

--- src/api/axios.jsx
+++ src/api/axios.jsx
@@ -5,3 +5,9 @@ export default axios.create({
 })
 
 //https://www.youtube.com/watch?v=nI8PYZNFtac
+// eu acho que não é a hora de aplicar isso, 
+
+//e colocar as tags para conseguir separar
+// é gasto com oq?
+// é investimento?
+//xD
