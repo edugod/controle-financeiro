e64f151dc2e4a7b4390fb90c6829069be342358c
Eduardo Lange <eduardoandrelange@gmail.com>
2024-04-07 12:00:55 -0300

    ok

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -53,46 +53,47 @@ const HomePage = () => {
 		localStorage.removeItem('token')
 		navigate('/login')
 	}
+	
 
-    return (
-        <div className='container bg-tertiary mx-auto mt-8 px-4 py-8 rounded-2xl flex flex-col items-center shadow-2xl relative'>
-            <div className='mb-8'>
-                <SaldoMensal despesas={despesas} usuario={usuario} />
-            </div>
-            <div className='mb-8'>
-                <HistoricoDespesas despesas={despesas} setDespesas={setDespesas} usuario={usuario} />
-            </div>
-            <div className='flex flex-col items-center'>
-                <button
-                    onClick={() => setShowDespesaForm(true)}
-                    className='bg-forth hover:bg-forthHover text-white font-bold py-2 px-4 rounded-2xl'
-                >
-                    Adicionar Despesa
-                </button>
-                {showDespesaForm && (
-                    <div className='fixed inset-0 z-50 overflow-y-auto flex justify-center items-center bg-gray-500 bg-opacity-50 transition-opacity'>
-                        <div className='relative bg-white rounded-2xl shadow-lg w-128 h-128'>
-                            <div className='absolute top-0 right-0 -mt-4 -mr-4'></div>
-                            <div className='p-6'>
-                                <DespesaForm
-                                    handleAddDespesa={handleAddDespesa}
-                                    usuario={usuario}
-                                    setShowDespesaForm={setShowDespesaForm}
-                                />
-                            </div>
-                            <div className='p-4'></div>
-                        </div>
-                    </div>
-                )}
-            </div>
-            <button
-                onClick={handleLogout}
-                className='bg-cancel hover:bg-cancelHover text-sm text-tertiary py-1 px-1 rounded-2xl absolute bottom-4 right-4'
-            >
-                Logout
-            </button>
-        </div>
-    )
+	return (
+		<div className='container bg-tertiary mx-auto mt-8 px-4 py-8 rounded-2xl flex flex-col items-center shadow-2xl relative'>
+			<div className='mb-8'>
+				<SaldoMensal despesas={despesas} usuario={usuario} />
+			</div>
+			<div className='mb-8'>
+				<HistoricoDespesas despesas={despesas} setDespesas={setDespesas} usuario={usuario} />
+			</div>
+			<div className='flex flex-col items-center'>
+				<button
+					onClick={() => setShowDespesaForm(true)}
+					className='bg-forth hover:bg-forthHover text-white font-bold py-2 px-4 rounded-2xl'
+				>
+					Adicionar Despesa
+				</button>
+				{showDespesaForm && (
+					<div className='fixed inset-0 z-50 overflow-y-auto flex justify-center items-center bg-gray-500 bg-opacity-50 transition-opacity'>
+						<div className='relative bg-white rounded-2xl shadow-lg w-128 h-128'>
+							<div className='absolute top-0 right-0 -mt-4 -mr-4'></div>
+							<div className='p-6'>
+								<DespesaForm
+									handleAddDespesa={handleAddDespesa}
+									usuario={usuario}
+									setShowDespesaForm={setShowDespesaForm}
+								/>
+							</div>
+							<div className='p-4'></div>
+						</div>
+					</div>
+				)}
+			</div>
+			<button
+				onClick={handleLogout}
+				className='bg-cancel hover:bg-cancelHover text-sm text-tertiary py-1 px-1 rounded-2xl absolute bottom-4 right-4'
+			>
+				Logout
+			</button>
+		</div>
+	)
 }
 
 export default HomePage
