92e9152b0d8b1e751444672e233c6791ec351170
Eduardo Lange <eduardoandrelange@gmail.com>
2024-04-14 15:40:01 -0300

    apenas dia/mês

--- src/Components/DespesaItem.jsx
+++ src/Components/DespesaItem.jsx
@@ -5,14 +5,15 @@ const DespesaItem = ({ despesa, onDelete }) => {
         onDelete(despesa.id);
     }, [despesa.id, onDelete]);
 
-    const { dia, valor, observacao, categoria } = despesa;
+    const { dia: data, valor, observacao, categoria } = despesa;
 
+    const [dia, mes, ano] = data.split('/')
 
     return (
         <li onClick={handleDelete} className='flex items-center justify-between py-2 border-b-2 border-primary cursor-pointer'>
             <div>
                 <span className='font-bold ml-2'></span> R$ {valor.toFixed(2)} |
-                 {' '}{dia} |
+                 {' '}{dia}/{mes} |
                 <span className='font-bold ml-2'>Motivo:</span> {observacao} |
                 <span className='font-bold ml-2'>Categoria:</span> {categoria}
             </div>
